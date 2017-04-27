3b71d778650cf13bb1c8d3522082bef78c56f7a8
Alok Puranik <alokpuranik1@gmail.com>
2017-04-27 13:06:40 -0400

    Add function to get ticker from id

--- backtest.r
+++ backtest.r
@@ -11,3 +11,8 @@ obelix.connect <- function(config_file) {
     connect_string <- sprintf(template, connect_params['driver'], connect_params['uid'], connect_params['pwd'], connect_params['db'])
     odbcDriverConnect(connect_string)
 }
+
+obelix.get_ticker <- function(dbconn, id) {
+    querystr <- sprintf('SELECT TOP 1 ticker FROM dim_equity WHERE id=%d', id)
+    sqlQuery(dbconn, querystr)[[1]]
+}
