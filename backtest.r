4716b87860e9c4fd552f5bea9e9854b3a263e51c
Alok Puranik <alokpuranik1@gmail.com>
2017-05-03 01:41:36 -0400

    Implement ALL the things

--- backtest.r
+++ backtest.r
@@ -33,8 +33,14 @@ obelix.get_beta <- function(dbconn, id, date) {
 }
 
 obelix.stocks_in_decile <- function(dbconn, decile, date) {
-    year <- as.numeric(format(date, "%Y"))
-    sqlQuery(dbconn, sprintf('SELECT id FROM decilemember WHERE y = %d AND decile = %d;', year, decile))
+    datestr <- as.numeric(format(date, "%Y%m%d"))
+    total_stocks <- sqlQuery(dbconn, sprintf('SELECT COUNT(DISTINCT id) FROM fact_equity WHERE d = \'%s\'', datestr))
+    print(total_stocks)
+    decile_size <- as.integer(total_stocks / 10)
+    template <- 'WITH NumberedCap AS (SELECT id, ROW_NUMBER() OVER (ORDER BY cap) AS caprank FROM fact_equity WHERE d = \'%s\') SELECT id FROM NumberedCap WHERE caprank BETWEEN %d AND %d'
+    res <- sqlQuery(dbconn, sprintf(template, datestr, (decile-1) * decile_size, decile * decile_size))
+    print(head(res))
+    return(res)
 }
 
 obelix.logret_over_period <- function(dbconn, ids, start, end) {
@@ -54,35 +60,50 @@ obelix.logret_daily <- function(dbconn, id, start, end) {
 
 obelix.market_cap <- function(dbconn, ids, date) {
     datestr <- format(date, '%Y%m%d')
-    querystr <- sprintf('SELECT id, cap FROM ??? WHERE id in (%s) AND d = %s',
+    querystr <- sprintf('SELECT id, cap FROM fact_equity WHERE id in (%s) AND d = \'%s\'',
                         paste(ids, collapse=','), datestr)
     sqlQuery(dbconn, querystr)
 }
 
 # compute log returns at constant weights
 # I think this function is actually entirely useless :(
-obelix.logret_daily <- function(dbconn, id_weights, start, end) {
-    returns <- lapply(rownames(id_weights),
-                      function(row) {
-                          id_weights[[row, 'weights']] *
-                              (exp(obelix.logret_daily(dbconn, id_weights[[row, 'ids']], start, end)) - 1)
-                      })
-    returns$all <- T
-    fullxts <- do.call(merge.xts, returns)
-    fullxts[is.na(fullxts)] <- 0
-    as.xts(log(1 + rowSums(fullxts, na.rm=T)), order.by=time(fullxts))
-}
+# obelix.logret_daily <- function(dbconn, id_weights, start, end) {
+#     returns <- lapply(rownames(id_weights),
+#                       function(row) {
+#                           id_weights[[row, 'weights']] *
+#                               (exp(obelix.logret_daily(dbconn, id_weights[[row, 'ids']], start, end)) - 1)
+#                       })
+#     returns$all <- T
+#     fullxts <- do.call(merge.xts, returns)
+#     fullxts[is.na(fullxts)] <- 0
+#     as.xts(log(1 + rowSums(fullxts, na.rm=T)), order.by=time(fullxts))
+# }
 
 dataprep.bond_prices <- function() {
     getSymbols('JNK', src="yahoo", auto.assign=F)$JNK.Adjusted
 }
 
-dataprep.spx_prices <- function() {
-    getSymbols('GSPC', src="yahoo", auto.assign=F)$GSPC.Adjusted
+obelix.spx_prices <- function(dbconn, startyear, endyear) {
+    res <- sqlQuery(dbconn, sprintf('SELECT Date, AdjClose from spx_yhoo WHERE Date BETWEEN \'%d\' AND \'%d\'', startyear, endyear+1))
+    print(head(res))
+    as.xts(res[ ,'AdjClose'], order.by=res[,'Date'])
 }
 
 dataprep.logret <- function(prices) {
-    log(diff(prices, arithmetic=F))
+    logret <- log(diff(prices, arithmetic=F))
+    logret[is.na(logret)] <- 0
+    return(logret)
+}
+
+portfolio.get_weights <- function(dbconn, date) {
+    ids <- do.call(c, sapply(10:10, obelix.stocks_in_decile, dbconn=dbconn, date=date))
+    mcaps <- obelix.market_cap(dbconn, ids, date)
+    print(head(mcaps))
+    mcaps <- na.omit(mcaps)
+    totalcap <- sum(mcaps[, 'cap'])
+    print(head(totalcap))
+    weights <- data.frame(ids=mcaps[, 'id'], weights=mcaps[, 'cap'] / totalcap)
+    return(weights)
 }
 
 # compute log returns, where weights start as specified and track value
@@ -93,6 +114,7 @@ portfolio.growth_of_dollar <- function(dbconn, id_weights, start, end) {
                       function(row) {
                           id_weights[[row, 'weights']] * exp(cumsum(obelix.logret_daily(dbconn, id_weights[[row, 'ids']], start, end)))
                       })
+    # print(head(prices_normalized))
     prices_normalized$all <- T
     fullprices <- do.call(merge.xts, prices_normalized)
     # initial price is p, ending price is p(1 + simple)
@@ -102,15 +124,16 @@ portfolio.growth_of_dollar <- function(dbconn, id_weights, start, end) {
     return(growth_of_dollar)
 }
 
-portfolio.logret_value_weight <- function(dbconn, id_weights, start, end) {
-    growth <- portfolio.growth_of_dollar(dbconn, id_weights, start, end)
-    logret <- log(diff(growth, arithmetic=F))
-    logret[is.na(logret)] <- 0
-    return(logret)
-}
-
 # compute holdings ratio such that the net beta is 1
 portfolio.holdings_ratio <- function(beta1, beta2) {
     (beta2 - 1) / (beta2 - beta1)
 }
 
+portfolio.rebalance_days <- function(timeseries, startyear, endyear) {
+    relevant <- timeseries[sprintf('%d/%d', startyear, endyear)]
+    monthstarts <- do.call(rbind, lapply(split(relevant, 'months'), first))
+    time(monthstarts[seq.int(1, length(monthstarts), 6)])
+}
+
+portfolio.small_cap_return <- function(refseries, startyear, endyear) {
+}
